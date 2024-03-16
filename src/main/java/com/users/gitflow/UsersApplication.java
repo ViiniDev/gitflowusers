a1f84ed57dddabb4eb2966c930c46820f9dd4613
ViiniDev <viniciusarruda189@gmail.com>
2024-03-16 15:18:18 -0300

    Iniciando projeto Cadastro de Users

---
+++ src/main/java/com/users/gitflow/UsersApplication.java
@@ -0,0 +1,13 @@
+package com.users.gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApplication.class, args);
+	}
+
+}
