9ed28a5b5668dbd608f87d84c4ea66585113cfb1
Mattheuzzera <freitas.matheus@mail.uft.edu.br>
2024-03-17 17:07:32 -0300

    adicionando metodo Get

--- src/main/java/com/users/gitflow/controllers/UsersController.java
+++ src/main/java/com/users/gitflow/controllers/UsersController.java
@@ -1,11 +1,17 @@
 package com.users.gitflow.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UsersController {
-
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        var allUsers = repository.findAll();
+        return ResponseEntity.ok(allUsers);
+    }
 
 }
